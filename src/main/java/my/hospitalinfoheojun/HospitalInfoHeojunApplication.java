e5947450508bae61b4e0fe235aab122cfb46cf52
bearjun05 <bearjun05@naver.com>
2022-11-03 19:21:23 +0900

    Initial commit

---
+++ src/main/java/my/hospitalinfoheojun/HospitalInfoHeojunApplication.java
@@ -0,0 +1,13 @@
+package my.hospitalinfoheojun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalInfoHeojunApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(HospitalInfoHeojunApplication.class, args);
+  }
+
+}
---
+++ src/test/java/my/hospitalinfoheojun/HospitalInfoHeojunApplicationTests.java
@@ -0,0 +1,13 @@
+package my.hospitalinfoheojun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalInfoHeojunApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
